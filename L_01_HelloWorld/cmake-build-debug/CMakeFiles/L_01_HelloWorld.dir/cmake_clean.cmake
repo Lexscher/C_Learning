f9bb76bb8ce4742df715aafb56fb859ef7958db5
Alexander Schelchere <ascellie@gmail.com>
2018-08-02 17:59:16 -0400

    Hello World!

---
+++ L_01_HelloWorld/cmake-build-debug/CMakeFiles/L_01_HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/L_01_HelloWorld.dir/main.c.o"
+  "L_01_HelloWorld.pdb"
+  "L_01_HelloWorld"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/L_01_HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ L_01_HelloWorld/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\nLet's do stuff here");
+    printf(" because today is Thursday, %i/%i/%i.", 8,2,2018);
+    return 0;
+}
